9a0960a0b4c278ee3e8e6d7e4b77d4f310576278
subbarao <subbarao@subbarao.local>
2012-08-13 09:39:05 -0400

    removing active record

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,9 +1,10 @@
 class ResourcesController < ApplicationController
+
   def search
     @resource = Resource.new(params[:resource])
     @resource.search
 
-    render json: @resource
+    render json: @resource.as_json(only: [:response_body, :response_code])
   end
 
   def index
